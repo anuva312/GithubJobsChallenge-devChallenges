36f556b10c4de9228c66545717f2d17571d3c63b
Anusree V A <anusreeva93@gmail.com>
2021-12-22 12:42:41 +0530

    Added data to mock api response

--- src/data.js
+++ src/data.js
@@ -7,6 +7,10 @@ let jobList = [
     location: "New York",
     fullTime: false,
     postedDate: new Date("12/03/2021"),
+    companyMailId: "compname@company.com",
+    hrMailId: "hr@company.com",
+    about:
+      "cdncwidonvwp dsvmwpvnwpdnvdpvnw dmcpwnvenvpewnvpwnvepivnepinvpeivnpevn smcpamcpacmpcpkpkppqwpnvepw\n owncowiecwioenciwnciwnciwnwoncwioeniwnioewnvoinvinviwoncoinwsic \n ncoiwn coiwenciowencvioewnioncew oinfbvufhgierhowind knvkansc jqsnjnxin aocdpvnwiniwd wkdnwk",
   },
   {
     id: "1234567810",
@@ -16,6 +20,10 @@ let jobList = [
     location: "New York",
     fullTime: true,
     postedDate: new Date("11/28/2021"),
+    companyMailId: "compname@company.com",
+    hrMailId: "hr@company.com",
+    about:
+      "cdncwidonvwp dsvmwpvnwpdnvdpvnw dmcpwnvenvpewnvpwnvepivnepinvpeivnpevn smcpamcpacmpcpkpkppqwpnvepw\n owncowiecwioenciwnciwnciwnwoncwioeniwnioewnvoinvinviwoncoinwsic \n ncoiwn coiwenciowencvioewnioncew oinfbvufhgierhowind knvkansc jqsnjnxin aocdpvnwiniwd wkdnwk",
   },
   {
     id: "1234567811",
@@ -25,6 +33,10 @@ let jobList = [
     location: "New York",
     fullTime: true,
     postedDate: new Date("12/08/2021"),
+    companyMailId: "compname@company.com",
+    hrMailId: "hr@company.com",
+    about:
+      "cdncwidonvwp dsvmwpvnwpdnvdpvnw dmcpwnvenvpewnvpwnvepivnepinvpeivnpevn smcpamcpacmpcpkpkppqwpnvepw\n owncowiecwioenciwnciwnciwnwoncwioeniwnioewnvoinvinviwoncoinwsic \n ncoiwn coiwenciowencvioewnioncew oinfbvufhgierhowind knvkansc jqsnjnxin aocdpvnwiniwd wkdnwk",
   },
   {
     id: "1234567812",
@@ -34,6 +46,10 @@ let jobList = [
     location: "New York",
     fullTime: true,
     postedDate: new Date("12/12/2021"),
+    companyMailId: "compname@company.com",
+    hrMailId: "hr@company.com",
+    about:
+      "cdncwidonvwp dsvmwpvnwpdnvdpvnw dmcpwnvenvpewnvpwnvepivnepinvpeivnpevn smcpamcpacmpcpkpkppqwpnvepw\n owncowiecwioenciwnciwnciwnwoncwioeniwnioewnvoinvinviwoncoinwsic \n ncoiwn coiwenciowencvioewnioncew oinfbvufhgierhowind knvkansc jqsnjnxin aocdpvnwiniwd wkdnwk",
   },
   {
     id: "1234567813",
@@ -43,9 +59,17 @@ let jobList = [
     location: "New York",
     fullTime: false,
     postedDate: new Date(),
+    companyMailId: "compname@company.com",
+    hrMailId: "hr@company.com",
+    about:
+      "cdncwidonvwp dsvmwpvnwpdnvdpvnw dmcpwnvenvpewnvpwnvepivnepinvpeivnpevn smcpamcpacmpcpkpkppqwpnvepw\n owncowiecwioenciwnciwnciwnwoncwioeniwnioewnvoinvinviwoncoinwsic \n ncoiwn coiwenciowencvioewnioncew oinfbvufhgierhowind knvkansc jqsnjnxin aocdpvnwiniwd wkdnwk",
   },
 ];
 
 export function getJobs() {
   return jobList;
+}
+
+export function getJobById(id) {
+  return jobList.find((job) => job.id === id);
 }
