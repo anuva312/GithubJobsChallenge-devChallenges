230a113c40a0fed42dc7ae381e4e29d2398eff5e
Anusree V A <anusreeva93@gmail.com>
2021-12-26 18:28:20 +0530

    Added functionality for location based search

--- src/App.js
+++ src/App.js
@@ -6,14 +6,20 @@ import JobCards from "./components/JobCards";
 import { getJobs } from "./data";
 import "./App.css";
 
-// const filterOptions = ["City", "Zip Code", "Country"];
-
 const customStyles = {
   control: (provided, state) => ({
     ...provided,
     boxShadow: "none",
     border: "none",
     flexDirection: "row-reverse",
+  }),
+  groupHeading: (provided) => ({
+    ...provided,
+    fontFamily: "Poppins",
+    fontStyle: "normal",
+    fontWeight: 500,
+    fontSize: "14px",
+    color: "#334680",
   }),
 };
 
@@ -22,6 +28,7 @@ function App() {
   const [isChecked, setIsChecked] = useState(false);
   const [titles, setTitles] = useState([]);
   const [search, setSearch] = useState();
+  const [groupedOptions, setGroupedOptions] = useState();
   const [selectedFilter, setSelectedFilter] = useState({});
   const [searchKeyword, setSearchKeyword] = useState({});
   const jobsList = getJobs();
@@ -29,37 +36,55 @@ function App() {
   useEffect(() => {
     const todaysDate = new Date();
     let _titles = [];
+    let cityOptions = [];
+    let zipCodeOptions = [];
+    let countryOptions = [];
     const _jobs = jobsList.map((job) => {
       var Difference_In_Time = todaysDate.getTime() - job.postedDate.getTime();
       job["days_ago"] = Math.round(Difference_In_Time / (1000 * 3600 * 24));
-      _titles.push({ label: job.jobName, value: job.id });
+      _titles.push(job.jobName);
+      cityOptions.push(job.location);
+      zipCodeOptions.push(job.zipCode);
+      countryOptions.push(job.country);
       return job;
     });
+    _titles = [...new Set(_titles)].map((option) => ({
+      label: option,
+      value: option,
+    }));
+    cityOptions = [...new Set(cityOptions)].map((option) => ({
+      label: option,
+      value: option,
+      type: "location",
+    }));
+    zipCodeOptions = [...new Set(zipCodeOptions)].map((option) => ({
+      label: option,
+      value: option,
+      type: "zipCode",
+    }));
+    countryOptions = [...new Set(countryOptions)].map((option) => ({
+      label: option,
+      value: option,
+      type: "country",
+    }));
     console.log(_jobs);
     setTitles(_titles);
+    setGroupedOptions([
+      {
+        label: "City",
+        options: cityOptions,
+      },
+      {
+        label: "Zip Code",
+        options: zipCodeOptions,
+      },
+      {
+        label: "Country",
+        options: countryOptions,
+      },
+    ]);
     setJobData(_jobs);
   }, [jobsList]);
-
-  // useEffect(() => {
-  //   if (jobData?.length) {
-  //     let options = [];
-  //     switch (selectedFilter) {
-  //       case "City":
-  //         options = jobData.map((job) => job.location);
-  //         break;
-  //       case "Zip Code":
-  //         options = jobData.map((job) => job.zipCode);
-  //         break;
-  //       case "Country":
-  //         options = jobData.map((job) => job.country);
-  //         break;
-  //       default:
-  //         options = jobData.map((job) => job.location);
-  //         break;
-  //     }
-  //     setSelectedFilterOptions([...new Set(options)]);
-  //   }
-  // }, [jobData, selectedFilter]);
 
   useEffect(() => {
     let _jobData = isChecked
@@ -75,8 +100,15 @@ function App() {
             )
         );
       } else {
-        _jobData = _jobData.filter((job) => job.id === searchKeyword.value);
+        _jobData = _jobData.filter(
+          (job) => job.jobName === searchKeyword.value
+        );
       }
+    }
+    if (selectedFilter.label) {
+      _jobData = _jobData.filter(
+        (job) => job[selectedFilter.type] === selectedFilter.value
+      );
     }
     setJobData(_jobData);
   }, [isChecked, searchKeyword, selectedFilter, jobsList]);
@@ -94,15 +126,28 @@ function App() {
       }
   };
 
-  const handleOnChangeSearch = function (e) {
-    console.log("Inside OnChange", e);
-    setSearch(e);
+  const handleOnChangeSearch = function (option) {
+    console.log("Inside OnChange", option);
+    setSearch(option);
+  };
+
+  const handleOnChangeFilter = function (option) {
+    console.log("Inside OnChange", option);
+    setSelectedFilter(option);
+  };
+
+  const DropdownIndicatorCreatable = (props) => {
+    return (
+      <components.DropdownIndicator {...props}>
+        <span className="material-icons">work_outline</span>
+      </components.DropdownIndicator>
+    );
   };
 
   const DropdownIndicator = (props) => {
     return (
       <components.DropdownIndicator {...props}>
-        <span className="material-icons">work_outline</span>
+        <span className="material-icons">public</span>
       </components.DropdownIndicator>
     );
   };
@@ -117,12 +162,11 @@ function App() {
         <div className="search-container">
           <CreatableSelect
             className="select-box"
-            classNamePrefix="select-box-items"
             styles={customStyles}
             onChange={handleOnChangeSearch}
             formatCreateLabel={(value) => `Search for "${value}"`}
             components={{
-              DropdownIndicator,
+              DropdownIndicator: DropdownIndicatorCreatable,
               IndicatorSeparator: () => null,
             }}
             name="search-keyword"
@@ -155,7 +199,18 @@ function App() {
           <div className="search-location-container">
             <div className="search-location-heading">LOCATION</div>
 
-            <div className="search-location-type">City</div>
+            <Select
+              className="select-box search-location-box"
+              styles={customStyles}
+              onChange={handleOnChangeFilter}
+              components={{
+                DropdownIndicator,
+                IndicatorSeparator: () => null,
+              }}
+              name="search-keyword"
+              options={groupedOptions}
+              placeholder="City, zip code or country"
+            ></Select>
           </div>
         </div>
         {jobData?.length ? (
@@ -172,7 +227,7 @@ function App() {
           </div>
         ) : (
           <div className="placeholder-job-cards">
-            <div>Sorry...No jobs that match the search criteria</div>
+            <div>Sorry... No jobs found that match the search criteria</div>
           </div>
         )}
       </div>
